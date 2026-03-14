acc41ff94202d5f335a998d02a4470bebebdfca6
Abdulhakim TUano <157072745+KimTuano@users.noreply.github.com>
2026-03-14 15:34:26 +0800

    Fix base path for production

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/CareQueuetAdmin-Thesis/',  // 👈 Change from '/carequeue-thesis/' to '/'
+  base: '/',
 })
